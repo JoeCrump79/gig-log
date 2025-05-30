a5148d5841d55444c88161bad5897b2bc9d41c73
Joseph Crump <josephcrump79@yahoo.com>
2025-05-30 13:17:17 -0500

    Import and render NavBar in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import NavBar from "./components/NavBar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Gig Log</h1>
+
     </div>
   );
 }
