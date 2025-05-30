b82906048480faf1bc0d65aadaf6358667e57c69
Joseph Crump <josephcrump79@yahoo.com>
2025-05-30 15:10:15 -0500

    Import and render ShowCard inside ShowList

--- src/components/ShowList.js
+++ src/components/ShowList.js
@@ -1,5 +1,10 @@
+import ShowCard from "./ShowCard";
 function ShowList() {
-  return <div>ShowList</div>;
+  return <div>
+  <h2>Show List</h2>
+  <ShowCard />
+</div>
+;
 }
 
 export default ShowList;
