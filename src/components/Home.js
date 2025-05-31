5f1936cebb89010f0358d2b70b2f596ee54219de
Joseph Crump <josephcrump79@yahoo.com>
2025-05-30 23:09:54 -0500

    Create Home component with welcome message

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Gig Log</h2>
+      <p>Use the navigation to view or add concerts you've attended.</p>
+    </div>
+  );
+}
+
+export default Home;
